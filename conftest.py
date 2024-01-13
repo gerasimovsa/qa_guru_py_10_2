6c26a3329f64f8359b153beeb39d832ebfd17f92
ra9_adept <gerasimov.ra9@gmail.com>
2024-01-13 22:13:23 +0400

    Updating both conftest and test_google_search

--- conftest.py
+++ conftest.py
@@ -2,13 +2,9 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture
-def browser_settings():
-    browser.driver.set_window_size(1920, 1200)
-
-
 @pytest.fixture(scope="function", autouse=True)
-def setup_and_teardown(browser_settings):
-    browser.open("https://google.com")
+def setup_and_teardown():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1200
     yield
     browser.quit()
--- test_google_search.py
+++ test_google_search.py
@@ -2,11 +2,13 @@ from selene import browser, be, have
 
 
 def test_google_search_valid_query():
+    browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_google_search_invalid_query():
     rand_string = "pdfogkjsbrdt54"
+    browser.open("https://google.com")
     browser.element('[name="q"]').should(be.blank).type(rand_string).press_enter()
     browser.element('[id="botstuff"]').should(have.text(f'По запросу {rand_string} ничего не найдено.'))
